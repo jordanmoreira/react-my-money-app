6b0de47be02cea72bde93a78d90092bf5e4ed097
Moreira <Jordan.Moreira@sovos.com>
2019-01-31 12:41:15 -0200

    routes refactor

--- backend/src/config/routes.js
+++ backend/src/config/routes.js
@@ -1,12 +1,41 @@
 const express = require('express')
+const auth = require('./auth')
 
 module.exports = function (server) {
 
-    // Definir URL base para todas as rotas
-    const router = express.Router()
-    server.use('/api', router)
+    // JWT token protected routes.
+    const protectedApi = express.Router()
+    server.use('/api', protectedApi)
 
-    // Rotas de ciclo de pagamento
+    protectedApi.use(auth)
+
     const BillingCycle = require('../api/billingCycle/billingCycleService')
-    BillingCycle.register(router, '/billingCycles')
+    BillingCycle.register(protectedApi, '/billingCycles')
+
+
+    // Open routes
+    const openApi = express.Router()
+    server.use('/oapi', openApi)
+
+    const AuthService = require('../api/user/authService')
+    openApi.post('/login', AuthService.login)
+    openApi.post('/signup', AuthService.signup)
+    openApi.post('/validateToken', AuthService.validateToken)
 }
+
+
+
+// Old version maintaned for study purposes only.
+
+// const express = require('express')
+
+// module.exports = function (server) {
+
+//     // Definir URL base para todas as rotas
+//     const router = express.Router()
+//     server.use('/api', router)
+
+//     // Rotas de ciclo de pagamento
+//     const BillingCycle = require('../api/billingCycle/billingCycleService')
+//     BillingCycle.register(router, '/billingCycles')
+// }
